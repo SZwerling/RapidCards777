1ad8bfe2ce62cb3799688f1ff05ead8bb04b1e91
unknown <sazwerling@gmail.com>
2023-05-24 14:33:14 -0700

    welcome page responsive styling

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -32,32 +32,33 @@ const Welcome = () => {
                   <source src={VideoSnipp} type="video/mp4" />
                </video>
             </div>
-            <div className="intro-text-1">
-               <p className="text-paragraph">
-                  Effortlessly create your own flashcards, tailor-made to suit
-                  your unique learning needs. Whether it's vocabulary, math
-                  equations, historical events, or scientific formulas,
-                  RapidCards777 lets you design flashcards that reflect your
-                  curriculum or personal study goals.
-               </p>
-               <p className="text-paragraph text-paragraph-hidden">
-                  Visualize complex concepts and make learning a multisensory
-                  experience. Harness the power of collaboration by sharing your
-                  flashcards with classmates, friends, or study groups.
-               </p>
-            </div>
-            <div className="intro-text-2">
-               <p className="text-paragraph">
-                  Sync across devices. Seamlessly access your flashcards
-                  anytime, anywhere. Whether you're studying on your smartphone,
-                  tablet, or computer, FlashMaster syncs your progress across
-                  devices, ensuring you can pick up right where you left off.
-               </p>
-               <p className="text-paragraph text-paragraph-hidden">
-                  Expand your knowledge through collective efforts and benefit
-                  from a diverse range of perspectives. Together, you can
-                  achieve more!
-               </p>
+            <div className="welcome-page-text">
+               <div className="intro-text-1">
+                  <h4 className="h4">Create and Customize Your Flashcards</h4>
+                  <p className="text-paragraph">
+                     Whether it's vocabulary, math equations, historical events,
+                     or scientific formulas, RapidCards777 lets you design
+                     flashcards that reflect your curriculum or personal study
+                     goals.
+                  </p>
+               </div>
+               <div className="intro-text-2">
+                  <h4 className="h4">Collaborative Learning</h4>
+                  <p className="text-paragraph">
+                     Expand your knowledge through collective efforts and
+                     benefit from a diverse range of perspectives. Together, you
+                     can achieve more!
+                  </p>
+               </div>
+               <div className="intro-text-3">
+                  <h4 className="h4">Sync across devices</h4>
+                  <p className="text-paragraph">
+                     Seamlessly access your flashcards anytime, anywhere.
+                     Whether you're studying on your smartphone, tablet, or
+                     computer, RapidCards777 syncs your progress across devices,
+                     ensuring you can pick up right where you left off.
+                  </p>
+               </div>
             </div>
          </div>
       </>
